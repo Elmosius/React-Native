1ee3618f2b357d765f5c7d81551c1355328c9520
Elmosius Suli <elmosius18@gmail.com>
2025-11-24 16:58:01 +0700

    feat: learn komponen text, image, styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,42 @@
 // rnfes -> untuk template react native
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Logo from '../assets/logo.png';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logo} />
+
+      <Text style={([styles.title], { color: 'red' })}>Home React Native</Text>
+      <Text style={{ marginTop: 10, marginBottom: 10 }}>Reading List App</Text>
+
+      <View style={styles.card}>
+        <Text>Haii, ini sebuah Card</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  card: {
+    backgroundColor: '#eee',
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.1)',
+    borderRadius: 10,
+    padding: 12,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
