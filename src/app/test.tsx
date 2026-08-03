912d16cdabcb3b00ef6c4872c178e9f93754d68d
Elmosius Suli <elmosius18@gmail.com>
2026-08-03 10:42:15 +0700

    feat: setup nativewind

---
+++ src/app/test.tsx
@@ -0,0 +1,9 @@
+import { ScrollView, Text } from 'react-native';
+
+export default function TestScreen() {
+  return (
+    <ScrollView>
+      <Text className='font-bold text-xl text-red-500'>Test</Text>
+    </ScrollView>
+  );
+}
